977650f24ebe956845e0da9bbc89992b1370868f
cleantted <seo.cleantted@gmail.com>
2023-05-13 01:10:42 +0900

    format

--- mylib/launch_osc_server.py
+++ mylib/launch_osc_server.py
@@ -1,6 +1,7 @@
 import argparse
-from pythonosc import dispatcher
-from pythonosc import osc_server
+
+from pythonosc import dispatcher, osc_server
+
 from mylib.positioning_system import PositioningSystem
 
 
@@ -8,16 +9,23 @@ from mylib.positioning_system import PositioningSystem
 def launch_osc_server(positioning_system: PositioningSystem):
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="127.0.0.1",help="The ip to listen on")
-    parser.add_argument("--port", type=int, default=9001,help="The port to listen on")
+    parser.add_argument(
+        "--ip",
+        default="127.0.0.1",
+        help="The ip to listen on")
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=9001,
+        help="The port to listen on")
     args = parser.parse_args()
 
     dpt = dispatcher.Dispatcher()
 
     dpt.map("/avatar/parameters/VPS", positioning_system.pys_switch, dpt)
-    dpt.map(f"/avatar/parameters/VPS/sat_*", positioning_system.satellite_osc_handler)
+    dpt.map(f"/avatar/parameters/VPS/sat_*",
+            positioning_system.satellite_osc_handler)
 
     server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dpt)
     print("Serving on {}".format(server.server_address))
     server.serve_forever()
-
--- mylib/plot_graph.py
+++ mylib/plot_graph.py
@@ -1,9 +1,12 @@
 import matplotlib
-matplotlib.use("TkAgg")
-import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.pyplot as plt
 import numpy as np
+
 from mylib.positioning_system import PositioningSystem
+
+matplotlib.use("TkAgg")
+
 
 class PlotGraph:
     def __init__(self, positioning_system: PositioningSystem):
@@ -22,10 +25,11 @@ class PlotGraph:
         self.text = plt.figtext(0.05, 0.05, '')
 
     def update(self, frame):
-        if self.positioning_system.datalist is not None and len(self.positioning_system.datalist.datalist) > 0:
-            self.x_data  = self.positioning_system.datalist.x_data()
-            self.y_data  = self.positioning_system.datalist.y_data()
-            self.z_data  = self.positioning_system.datalist.z_data()
+        if self.positioning_system.datalist is not None and len(
+                self.positioning_system.datalist.datalist) > 0:
+            self.x_data = self.positioning_system.datalist.x_data()
+            self.y_data = self.positioning_system.datalist.y_data()
+            self.z_data = self.positioning_system.datalist.z_data()
             self.ax.clear()
             self.ax.set_xlim(-100, 100)
             self.ax.set_ylim(-100, 100)
@@ -36,14 +40,21 @@ class PlotGraph:
             self.ax.view_init(elev=90, azim=-90)  # Adjust the viewing angle
 
             # Change color intensity based on y-coordinate
-            for i in range(len(self.x_data)-1):
-                color_intensity = np.interp(self.y_data[i], (-100, 100), (0, 1))
-                self.ax.plot(self.x_data[i:i+2], self.z_data[i:i+2], self.y_data[i:i+2], color=plt.cm.Blues(color_intensity), lw=2)
-            
-            self.text.set_text(self.text_template.format(self.x_data[0], self.y_data[0], self.z_data[0]))
+            for i in range(len(self.x_data) - 1):
+                color_intensity = np.interp(
+                    self.y_data[i], (-100, 100), (0, 1))
+                self.ax.plot(self.x_data[i:i + 2],
+                             self.z_data[i:i + 2],
+                             self.y_data[i:i + 2],
+                             color=plt.cm.Blues(color_intensity),
+                             lw=2)
+
+            self.text.set_text(
+                self.text_template.format(
+                    self.x_data[0],
+                    self.y_data[0],
+                    self.z_data[0]))
 
     def start_plot_graph(self):
         ani = animation.FuncAnimation(self.fig, self.update, interval=100)
         plt.show()
-
-
